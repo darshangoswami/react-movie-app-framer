7716944a6f1e151433727ae31af008eb7e1ba6ba
darshangoswami <goswamidarshan999@gmail.com>
2022-07-13 13:28:08 +0530

    added loading data state

--- src/App.js
+++ src/App.js
@@ -8,14 +8,19 @@ function App() {
   const [popular, setPopular] = useState([]);
   const [filtered, setFiltered] = useState([]);
   const [activeGenre, setActiveGenre] = useState(0);
+  const [isLoading, setIsLoading] = useState(false);
 
   const fetchPopular = useCallback(async () => {
+    setIsLoading(true);
+
     const data = await fetch(
       "https://api.themoviedb.org/3/movie/popular?api_key=05aa490e85167baf705cb34e0602f814&language=en-US&page=1"
     );
     const movies = await data.json();
     setPopular(movies.results);
     setFiltered(movies.results);
+
+    setIsLoading(false);
   }, []);
 
   useEffect(() => {
@@ -32,9 +37,12 @@ function App() {
       />
       <motion.div layout className="popular-movies">
         <AnimatePresence>
+          {!isLoading && filtered.length > 0} &&
           {filtered.map((movie) => {
             return <Movie key={movie.id} movie={movie} />;
           })}
+          {!isLoading && filtered.length === 0 && <p>No movies found.</p>}
+          {isLoading && <p>Loading...</p>}
         </AnimatePresence>
       </motion.div>
     </div>
